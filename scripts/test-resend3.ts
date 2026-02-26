d913923abee9bbd5f020b5edb472110f474f1c3d
SH20RAJ <sh20raj.noteyard@blogger.com>
2026-02-26 20:44:26 +0530

    feat: add test scripts for form and user email notifications

--- scripts/test-resend3.ts
+++ scripts/test-resend3.ts
@@ -1,5 +1,5 @@
-import { db } from "./src/db";
-import { forms, users } from "./src/db/schema";
+import { db } from "../src/db";
+import { forms, users } from "../src/db/schema";
 import { eq } from "drizzle-orm";
 
 async function main() {
